f246f23b59fd21a0c7c7f3a902cb42d8a8652ae0
Kazi Tanvirul Islam Sakib <ktisakib@gmail.com>
2025-06-19 21:24:09 +0000

    init :  task-1 impilimenting the ui

---
+++ apps/task-1/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@workspace/ui/components/button"
+
+export default function Page() {
+  return (
+    <main className="flex items-center justify-center min-h-svh">
+
+    </main>
+  )
+}
